79720a06b0d6e89eb489a0dc6db91a2edd66ded2
Shivamgaikwad0077 <indianshivam077@gmail.com>
2026-08-19 22:12:37 +0530

    few changes

---
+++ operators.py
@@ -0,0 +1,58 @@
+print("Types of oprators in python  : ")
+print(""" 
+Types of operators in python 
+        │
+        ├── 1. Arithmetic operators
+        ├── 2. Comparison operators
+        ├── 3. Assignment operators
+        ├── 4. Logical operators
+        ├── 5. Identity operators
+        ├── 6. Membership operators
+        └── 7. Bitwise operators
+""")
+print("___________________________________________________________")
+print("1. Arithmetic oprators :  + , - , * , / , // , % , ** : ")
+a = 10
+b = 20 
+print("a + b = " , a+b)
+print("a - b =", a-b)
+print("a * b =" , a*b)
+print("a / b = ", a/b)
+print("a // b = ", a//b)
+print("a % b = ", a%b)
+print("a ** b = ", a**b)
+
+print("___________________________________________________________")
+print("2. Comparison oprators :  == , != , > , < , >= , <= : ")
+print(f"{a} == {b} ", "=" ,a==b)
+print(f"{a} != {b} ", "=" ,a !=b)
+print(f"{a} > {b} ", "=" ,a > b)
+print(f"{a} < {b} ", "=" ,a < b)
+print(f"{a} >= {b} ", "=" ,a >= b)
+print(f"{a} <= {b} ", "=" ,a <= b)
+
+print("___________________________________________________________")
+print("2. Comparison oprators :  += , -= , *= , /= , //= , *= , **= , &=, ^= , <<= , >>= : ")
+
+
+s = 5
+s **= 5
+print(f"{s} **=5 " , "=" , s) 
+
+x = 10
+x += 5
+print('x += 5 ', "=" , x)
+
+x -= 5
+print("x -= 5" , "=" , x)
+
+x *= 5
+print("x *= 5" , "=" , x)
+
+
+x /= 5
+print("x /= 5" , "=" , x)
+
+x //= 5
+print("x //= 5" , "=" , x)
+
