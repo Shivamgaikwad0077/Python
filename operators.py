339e301c813f232e9b824d772e747e709a8ccdb6
Shivamgaikwad0077 <indianshivam077@gmail.com>
2026-08-21 20:28:37 +0530

    added identity  oprators

--- operators.py
+++ operators.py
@@ -32,7 +32,7 @@ print(f"{a} >= {b} ", "=" ,a >= b)
 print(f"{a} <= {b} ", "=" ,a <= b)
 
 print("___________________________________________________________")
-print("2. Comparison oprators :  += , -= , *= , /= , //= , *= , **= , &=, ^= , <<= , >>= : ")
+print("3. Assignments oprators :  += , -= , *= , /= , //= , *= , **= , &=, ^= , <<= , >>= : ")
 
 
 s = 5
@@ -56,3 +56,38 @@ print("x /= 5" , "=" , x)
 x //= 5
 print("x //= 5" , "=" , x)
 
+print("___________________________________________________________")
+print("4. logical oprators : ")
+
+n = 10
+print( f" '({n} > 5 and {n} < 20)' " , "=" ,n > 5 and n < 20)
+
+print( f" '({n} > 5 or {n} < 20)' " , "=" ,n > 5 or n < 20)
+
+print(f"'(not {n} > 5)'", "=", not n > 5)
+
+print("___________________________________________________________")
+print("5 : Membership oprators  : in , not in ")
+
+fruits = ['apple' , "banana" , "mango"]
+print("apple" in fruits)
+print("orange" in fruits)
+print("orange" not in fruits)
+print("apple" not in fruits)
+
+name = "Python"
+
+print("P" in name)
+print("z" in name)
+print("z" not in name)
+
+print("___________________________________________________________")
+print("6 : identity oprators  : is , is not ")
+
+a = [10,20,30]
+b = a
+c = [34,55,22]
+print(a is b) #it will return true 
+print(a is not b)# it wil return false
+print(a is c )#it will return falase
+print(b is a) # it will return true beacuse a is in b
